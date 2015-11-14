ff95e96caea6052a3592132bb6367f85f72f4fd1
yzhizai <duansf@ihep.ac.cn>
2015-11-14 20:43:13 +0800

    modify the name rules

--- dicom_category_cfg_run_func.m
+++ dicom_category_cfg_run_func.m
@@ -3,14 +3,17 @@ function dicom_category_cfg_run_func(job)
 folder_name = job.dirofDicom;
 outputDir = job.dirtoOutput;
 
-datasets = dicom_folder_info(folder_name{1}, true);
+for bb = 1:numel(folder_name)
+    datasets = dicom_folder_info(folder_name{bb}, true);
 
-for aa = 1:numel(datasets)
-    subFolderName = fullfile(outputDir{1}, datasets(aa).SeriesDescription);
-    if ~exist(subFolderName, 'dir')  
-        mkdir(subFolderName);
+    for aa = 1:numel(datasets)
+        subName = sprintf('%05d_%s', datasets(aa).DicomInfo.SeriesNumber, datasets(aa).DicomInfo.ProtocolName);
+        subFolderName = fullfile(outputDir{1}, datasets(aa).DicomInfo.PatientName.FamilyName, subName);
+        if ~exist(subFolderName, 'dir')  
+            mkdir(subFolderName);
+        end
+    %     destination = cell(size(datasets(aa).Filenames));
+    %     destination{:} = subFolderName;
+        cellfun(@(x)copyfile(x, subFolderName), datasets(aa).Filenames);
     end
-%     destination = cell(size(datasets(aa).Filenames));
-%     destination{:} = subFolderName;
-    cellfun(@(x)copyfile(x, subFolderName), datasets(aa).Filenames);
 end
