ee35b568fbcae7e23fa3a459c1e4221f68f6bdd5
yzhizai <dsf_ihep@yeah.net>
2016-08-31 10:01:33 +0800

    bug fix for un-dicom files

--- dicom_category_cfg_run_func.m
+++ dicom_category_cfg_run_func.m
@@ -7,13 +7,15 @@ for bb = 1:numel(folder_name)
     datasets = dicom_folder_info(folder_name{bb}, true);
 
     for aa = 1:numel(datasets)
-        subName = sprintf('%05d_%s', datasets(aa).DicomInfo.SeriesNumber, datasets(aa).DicomInfo.ProtocolName);
-        subFolderName = fullfile(outputDir{1}, datasets(aa).DicomInfo.PatientName.FamilyName, subName);
-        if ~exist(subFolderName, 'dir')  
-            mkdir(subFolderName);
+        try
+            subName = sprintf('%05d_%s', datasets(aa).DicomInfo.SeriesNumber, datasets(aa).DicomInfo.ProtocolName);
+            subFolderName = fullfile(outputDir{1}, datasets(aa).DicomInfo.PatientName.FamilyName, subName);
+            if ~exist(subFolderName, 'dir')  
+                mkdir(subFolderName);
+            end
+        %     destination = cell(size(datasets(aa).Filenames));
+        %     destination{:} = subFolderName;
+            cellfun(@(x)copyfile(x, subFolderName), datasets(aa).Filenames);
         end
-    %     destination = cell(size(datasets(aa).Filenames));
-    %     destination{:} = subFolderName;
-        cellfun(@(x)copyfile(x, subFolderName), datasets(aa).Filenames);
     end
 end
